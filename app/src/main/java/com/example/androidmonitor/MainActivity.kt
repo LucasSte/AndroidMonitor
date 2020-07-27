8fb84d1eea1ae6da65d9a15d801005a6a33f6b0e
vidalmatheus <matheusvidaldemenezes@gmail.com>
2020-07-26 22:49:29 -0300

    current thread

--- app/src/main/java/com/example/androidmonitor/MainActivity.kt
+++ app/src/main/java/com/example/androidmonitor/MainActivity.kt
@@ -4,8 +4,12 @@ import android.app.ActivityManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import java.io.BufferedReader
+import java.io.IOException
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         getPath()
         goToGpuPage()
+        getCpuUsage()
+        getThread()
         getMemoryUsage()
         getProcessName()
         getMyPID()
@@ -80,6 +86,78 @@ class MainActivity : AppCompatActivity() {
         // TODO: Implements
     }
 
+    private fun getCpuUsage()
+    {
+        var cpuUsage = getCpuUsageStatistic()
+        totalValue.text = cpuUsage!![1].toString()
+        runningValue.text = cpuUsage[3].toString()
+        sleepingValue.text = cpuUsage[5].toString()
+        stoppedValue.text = cpuUsage[7].toString()
+        zombieValue.text = cpuUsage[9].toString()
+    }
+
+    /**
+     *
+     * @return integer Array with 4 elements: user, system, idle and other cpu
+     * usage in percentage.
+     */
+    private fun getCpuUsageStatistic(): IntArray? {
+        var tempString = executeTop()
+        tempString = tempString!!.replace(",".toRegex(), "")
+        tempString = tempString.replace("User".toRegex(), "")
+        tempString = tempString.replace("System".toRegex(), "")
+        tempString = tempString.replace("IOW".toRegex(), "")
+        tempString = tempString.replace("IRQ".toRegex(), "")
+        tempString = tempString.replace("%".toRegex(), "")
+        for (i in 0..9) {
+            tempString = tempString!!.replace("  ".toRegex(), " ")
+        }
+        tempString = tempString!!.trim { it <= ' ' }
+        var myString = tempString.split(" ".toRegex()).toTypedArray()
+        print(myString)
+        var cpuUsageAsInt = IntArray(myString.size)
+        for (i in myString.indices) {
+            if (myString[i].length == 1) {
+                myString[i] = myString[i].trim { it <= ' ' }
+                cpuUsageAsInt[i] = myString[i].toInt()
+            }
+        }
+        return cpuUsageAsInt
+    }
+
+    private fun executeTop(): String? {
+        var p: Process? = null
+        var `in`: BufferedReader? = null
+        var returnString: String? = null
+        try {
+            p = Runtime.getRuntime().exec("top -n 1")
+            `in` = BufferedReader(InputStreamReader(p.inputStream))
+            while (returnString == null || returnString.contentEquals("")) {
+                returnString = `in`.readLine()
+            }
+        } catch (e: IOException) {
+            Log.e("executeTop", "error in getting first line of top")
+            e.printStackTrace()
+        } finally {
+            try {
+                `in`!!.close()
+                p!!.destroy()
+            } catch (e: IOException) {
+                Log.e(
+                    "executeTop",
+                    "error in closing and destroying top process"
+                )
+                e.printStackTrace()
+            }
+        }
+        return returnString
+    }
+
+    private fun getThread() {
+        currentThread.text = Thread.currentThread().toString()
+        currentThreadCount.text = Thread.activeCount().toString()
+    }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         getMemoryUsage()
@@ -88,5 +166,7 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         this.getMemoryUsage()
+        this.getCpuUsage()
+        this.getThread()
     }
 }
