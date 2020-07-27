76fe7e69842b4c26fdf0e4ca60e3350086b7a333
Lucas <lucas.tnagel@gmail.com>
2020-07-27 18:24:00 -0300

    Adding total ram and screen resolution

--- app/src/main/java/com/example/androidmonitor/StaticInfo.kt
+++ app/src/main/java/com/example/androidmonitor/StaticInfo.kt
@@ -1,12 +1,43 @@
 package com.example.androidmonitor
 
+import android.app.Activity
+import android.app.ActivityManager
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.DisplayMetrics
+import kotlinx.android.synthetic.main.activity_static_infos.*
 
 class StaticInfo : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_static_infos)
+
+        goBack()
+        loadTotalRam()
+        getScreenResolution()
+    }
+
+    private fun goBack()
+    {
+        goBack.setOnClickListener {
+            finish()
+        }
+    }
+
+    private fun loadTotalRam()
+    {
+        val mi = ActivityManager.MemoryInfo()
+        val activityManager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
+        activityManager.getMemoryInfo(mi)
+        totalRamTv.text = (mi.totalMem.toDouble() / 1073741824).toString()
+    }
+
+    private fun getScreenResolution()
+    {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+        displayResTv.text = displayMetrics.widthPixels.toString() + " X " + displayMetrics.heightPixels.toString()
     }
 }
