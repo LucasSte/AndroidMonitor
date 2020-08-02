56952c8569e83c0ea1efec2028511fa5f5af5ca5
omsf <ocimar.acad@gmail.com>
2020-08-02 18:47:51 -0300

    Add CPU name

--- app/src/main/java/com/example/androidmonitor/CpuInfo.kt
+++ app/src/main/java/com/example/androidmonitor/CpuInfo.kt
@@ -1,10 +1,12 @@
 package com.example.androidmonitor
 
 import android.os.Build
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_cpu_info.*
+import java.io.BufferedReader
+import java.io.FileReader
 
 class CpuInfo : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
@@ -13,6 +15,7 @@ class CpuInfo : AppCompatActivity() {
         setContentView(R.layout.activity_cpu_info)
         getNumberOfCores()
         getAbi()
+        getHardwareName()
         goBack()
     }
 
@@ -43,5 +46,49 @@ class CpuInfo : AppCompatActivity() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     fun getAbi() {
         abiTV.text = Build.SUPPORTED_ABIS[0]
+    }
+
+    fun getHardwareName(){
+        val br = BufferedReader(FileReader("/proc/cpuinfo"))
+
+        var str: String? = null
+
+        val output: MutableMap<String, String> = HashMap()
+
+        while (br.readLine().also({ str = it }) != null) {
+            val data = str?.split(":".toRegex())?.toTypedArray()
+            if (data != null) {
+                if (data.size > 1) {
+                    var key = data[0].trim { it <= ' ' }.replace(" ", "_")
+                    if (key == "Hardware") key = "cpu_model"
+                    output[key] = data[1].trim { it <= ' ' }
+                }
+            }
+        }
+        br.close()
+        cpuNameTV.text = output["cpu_model"]
+
+    }
+
+    fun getSwap(){
+        val br = BufferedReader(FileReader("/proc/cpuinfo"))
+
+        var str: String? = null
+
+        val output: MutableMap<String, String> = HashMap()
+
+        while (br.readLine().also({ str = it }) != null) {
+            val data = str?.split(":".toRegex())?.toTypedArray()
+            if (data != null) {
+                if (data.size > 1) {
+                    var key = data[0].trim { it <= ' ' }.replace(" ", "_")
+                    if (key == "Hardware") key = "cpu_model"
+                    output[key] = data[1].trim { it <= ' ' }
+                }
+            }
+        }
+        br.close()
+        cpuNameTV.text = output["cpu_model"]
+
     }
 }
